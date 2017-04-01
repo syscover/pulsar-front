497610c2230d2d593a0441d269efc0d12070781e
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-01 17:46:54 +0200

    add new menu

--- src/app/shared/components/side-navbar/side-navbar.component.ts
+++ src/app/shared/components/side-navbar/side-navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import * as Ps from 'perfect-scrollbar';
 
 @Component({
-  selector: 'ps-side-navbar',
-  templateUrl: './side-navbar.component.html',
-  styleUrls: ['./side-navbar.component.css']
+    selector: 'ps-side-navbar',
+    templateUrl: './side-navbar.component.html',
+    styleUrls: ['./side-navbar.component.css']
 })
 export class SideNavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const el = document.querySelector('.custom-scrollbar');
+        Ps.initialize(<HTMLElement>el);
+        $('.button-collapse').sideNav();
+        $('.collapsible').collapsible();
+    }
 
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,7 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+declare var jQuery:any; // jQuery definition
+declare var $:any; // jQuery definition
+declare module 'perfect-scrollbar'; // perfect-scrollbar to side-navbar
