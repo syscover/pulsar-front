86000ad6e6da62e19f2e2e4117c9c748c590e7c0
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-05 14:53:52 +0200

    reuse routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouteReuseStrategy } from '@angular/router';
+import { CustomReuseStrategy } from './shared/router/custom-reuse-srtrategy';
+
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +24,9 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     CoreModule
   ],
-  providers: [],
+  providers: [
+      //{ provide: RouteReuseStrategy, useClass: CustomReuseStrategy }
+  ],
   bootstrap: [
     AppComponent
   ]
