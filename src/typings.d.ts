255581dc1a36b3f41b6f024f266e9b6090d88aa3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-05-18 23:54:49 +0200

    set jQuery like local variable

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,6 +4,8 @@ interface NodeModule {
   id: string;
 }
 
-declare var jQuery:any; // jQuery definition
-declare var $:any; // jQuery definition
+// to declare jquery like global
+//declare var jQuery:any; // jQuery definition
+//declare var $:any; // jQuery definition
+
 declare module 'perfect-scrollbar'; // perfect-scrollbar to side-navbar
