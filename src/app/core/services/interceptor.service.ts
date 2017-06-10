956e8369dd07ca1a9317c87bcc7b7e8ca7fbef7e
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-06-10 19:32:58 +0200

    comments

--- src/app/core/services/interceptor.service.ts
+++ src/app/core/services/interceptor.service.ts
@@ -7,14 +7,19 @@ export class InterceptorXHRBackend extends XHRBackend {
 
     createConnection(request: Request): XHRConnection {
         let connection: XHRConnection = super.createConnection(request);
+
+        // pass callback to response to take toke from server and save in local storage
         connection.response = connection.response.do(this.processResponse);
+        
         return connection;
     }
 
-    processResponse(response: Response) {
+    // 
+    private processResponse(response: Response) {
         let authorization = response.headers.get('Authorization');
 
         if (authorization) {
+            // segment string to avoid Bearer word, the header has this format 'Bearer eyJ0eXAiOiJKV1QiLCJh...'
             let token = authorization.split(' ');
             localStorage.setItem('token', token[1]);
         }
