7effaed89f078d1135b64505ce4f178e83492b1c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-12 19:01:46 +0200

    add navigation from packages

--- src/app/main/content/core/services/navigation.service.ts
+++ src/app/main/content/core/services/navigation.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { navigation } from 'app/navigation/navigation';
+import { ConfigService } from './config.service';
 import { environment } from './../../../../../environments/environment';
+import * as _ from 'lodash';
 
 @Injectable()
 export class NavigationService
 {
     navigation: any;
 
-    constructor() 
+    constructor(
+        private configService: ConfigService
+    ) 
     {
         // Navigation data
         this.navigation = navigation;
@@ -15,8 +19,19 @@ export class NavigationService
 
     getNavigation()
     {
-        const packageMenu = this.navigation[0].children;
-        console.log(this.navigation[0].children);
+        const packages  = this.configService.get('packages');
+        const menuItems = [];
+        
+        for (const item of this.navigation[0].children) 
+        {     
+            const pkg = _.find(packages, {root: item.id});
+            if (pkg && pkg.active) menuItems.push(item);
+        }
+
+        if (environment.debug) console.log('DEBUG - menu intens: ', menuItems);
+
+        this.navigation[0].children = menuItems;
+        
         return this.navigation;
     }
 }
