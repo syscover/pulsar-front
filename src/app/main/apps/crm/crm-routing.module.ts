72ba2b8cd78bfcf68885f86ddca78fddd4fa98d6
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-02-07 13:09:43 +0100

    add permissions

--- src/app/main/apps/crm/crm-routing.module.ts
+++ src/app/main/apps/crm/crm-routing.module.ts
@@ -15,19 +15,19 @@ const routes: Routes = [
         canActivate: [AuthorizationService],
         children: [
             // Customers
-            { path: 'customer',                         component: CustomerListComponent,           data: { action: 'list' }},
-            { path: 'customer/create',                  component: CustomerDetailComponent,         data: { action: 'create' }},
-            { path: 'customer/show/:id',                component: CustomerDetailComponent,         data: { action: 'edit' }},
+            { path: 'customer',                         component: CustomerListComponent,           data: { action: 'list', resource: 'crm-customer' }},
+            { path: 'customer/create',                  component: CustomerDetailComponent,         data: { action: 'create', resource: 'crm-customer' }},
+            { path: 'customer/show/:id',                component: CustomerDetailComponent,         data: { action: 'edit', resource: 'crm-customer' }},
 
             // Groups
-            { path: 'customer-group',                   component: CustomerGroupListComponent,      data: { action: 'list' }},
-            { path: 'customer-group/create',            component: CustomerGroupDetailComponent,    data: { action: 'create' }},
-            { path: 'customer-group/show/:id',          component: CustomerGroupDetailComponent,    data: { action: 'edit' }},
+            { path: 'customer-group',                   component: CustomerGroupListComponent,      data: { action: 'list', resource: 'crm-group' }},
+            { path: 'customer-group/create',            component: CustomerGroupDetailComponent,    data: { action: 'create', resource: 'crm-group' }},
+            { path: 'customer-group/show/:id',          component: CustomerGroupDetailComponent,    data: { action: 'edit', resource: 'crm-group' }},
 
             // Address Types
-            { path: 'address-type',                     component: AddressTypeListComponent,        data: { action: 'list' }},
-            { path: 'address-type/create',              component: AddressTypeDetailComponent,      data: { action: 'create' }},
-            { path: 'address-type/show/:id',            component: AddressTypeDetailComponent,      data: { action: 'edit' }},
+            { path: 'address-type',                     component: AddressTypeListComponent,        data: { action: 'list', resource: 'crm-type' }},
+            { path: 'address-type/create',              component: AddressTypeDetailComponent,      data: { action: 'create', resource: 'crm-type' }},
+            { path: 'address-type/show/:id',            component: AddressTypeDetailComponent,      data: { action: 'edit', resource: 'crm-type' }},
         ]  
     }
 ];
