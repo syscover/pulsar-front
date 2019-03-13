5a8baae084406a5f4169ce1d65d9acf31ff001d1
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-13 19:04:37 +0100

    forem province

---
+++ src/app/main/apps/forem/province/province.graphql.ts
@@ -0,0 +1,60 @@
+import gql from 'graphql-tag';
+
+const fields = `
+    id
+    code
+    name
+`;
+
+const relationsFields = ``;
+
+export const graphQL = {
+    model: 'Syscover\\Forem\\Models\\Province',
+    table: 'forem_category',
+    fields,
+    relationsFields,
+
+    queryPaginationObject: gql`
+        query ForemGetProvincesPagination ($sql:[CoreSQLInput]) {
+            coreObjectsPagination: foremProvincesPagination (sql:$sql) {
+                total
+                objects (sql:$sql)
+                filtered
+            }
+        }`,
+
+    queryObjects: gql`
+        query ForemGetProvinces ($sql:[CoreSQLInput]) {
+            coreObjects: foremProvinces (sql:$sql) {
+                ${fields}
+            }
+        }`,
+
+    queryObject: gql`
+        query ForemGetProvince ($sql:[CoreSQLInput]) {
+            coreObject: foremProvince (sql:$sql) {
+                ${fields}
+            }
+        }`,
+
+    mutationCreateObject: gql`
+        mutation ForemCreateProvince ($payload:ForemProvinceInput!) {
+            foremCreateProvince (payload:$payload) {
+                ${fields}
+            }
+        }`,
+
+    mutationUpdateObject: gql`
+        mutation ForemUpdateProvince ($payload:ForemProvinceInput!) {
+            foremUpdateProvince (payload:$payload) {
+                ${fields}
+            }
+        }`,
+
+    mutationDeleteObject: gql`
+        mutation ForemDeleteProvince ($id:Int!) {
+            foremDeleteProvince (id:$id) {
+                ${fields}
+            }
+        }`
+};
