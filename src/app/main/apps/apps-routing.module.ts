53283ae9f2b1df3143f59607e051a2aa8c443a28
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-04-15 21:05:41 +0200

    create app-routing

---
+++ src/app/main/apps/apps-routing.module.ts
@@ -0,0 +1,82 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthorizationService } from '@horus/services/authorization.service';
+
+const routes = [
+    {
+        path        : 'auth',
+        loadChildren: './auth/auth.module#AuthModule',
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path        : 'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'admin',
+        loadChildren: './admin/admin.module#AdminModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'update',
+        loadChildren: './update/update.module#UpdateModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'cms',
+        loadChildren: './cms/cms.module#CmsModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'crm',
+        loadChildren: './crm/crm.module#CrmModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'market',
+        loadChildren: './market/market.module#MarketModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'wine',
+        loadChildren: './wine/wine.module#WineModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'review',
+        loadChildren: './review/review.module#ReviewModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'forem',
+        loadChildren: './forem/forem.module#ForemModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'bcci',
+        loadChildren: './bcci/bcci.module#BcciModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'peci',
+        loadChildren: './peci/peci.module#PeciModule',
+        canLoad     : [AuthorizationService]
+    },
+    {
+        path        : 'innova-concrete',
+        loadChildren: './innova-concrete/innova-concrete.module#InnovaConcreteModule',
+        canLoad     : [AuthorizationService]
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class AppsRoutingModule {}
--- src/app/main/apps/apps.module.ts
+++ src/app/main/apps/apps.module.ts
@@ -1,89 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MatPaginatorIntl } from '@angular/material';
-import { AuthorizationService } from '@horus/services/authorization.service';
 import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
-import { CustomMatPaginatorIntlService } from '@horus/services/custom-mat-paginator-int.service';
+import { AppsRoutingModule } from './apps-routing.module';
 import { locale as english } from './i18n/en';
 import { locale as spanish } from './i18n/es';
 
-const routes = [
-    {
-        path        : 'auth',
-        loadChildren: './auth/auth.module#AuthModule',
-    },
-    {
-        path        : 'dashboard',
-        loadChildren: './dashboard/dashboard.module#DashboardModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'admin',
-        loadChildren: './admin/admin.module#AdminModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'update',
-        loadChildren: './update/update.module#UpdateModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'cms',
-        loadChildren: './cms/cms.module#CmsModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'crm',
-        loadChildren: './crm/crm.module#CrmModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'market',
-        loadChildren: './market/market.module#MarketModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'wine',
-        loadChildren: './wine/wine.module#WineModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'review',
-        loadChildren: './review/review.module#ReviewModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'forem',
-        loadChildren: './forem/forem.module#ForemModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'bcci',
-        loadChildren: './bcci/bcci.module#BcciModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'peci',
-        loadChildren: './peci/peci.module#PeciModule',
-        canLoad     : [AuthorizationService]
-    },
-    {
-        path        : 'innova-concrete',
-        loadChildren: './innova-concrete/innova-concrete.module#InnovaConcreteModule',
-        canLoad     : [AuthorizationService]
-    },
-];
-
-
 @NgModule({
-    imports     : [
-        RouterModule.forChild(routes)
-    ],
-    declarations: [],
-    providers: [{
-        provide: MatPaginatorIntl, 
-        useClass: CustomMatPaginatorIntlService
-    }]
+    imports: [
+        AppsRoutingModule
+    ]
 })
 export class AppsModule
 {
