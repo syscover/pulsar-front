41e27aa6de894e86b0b5ee9a8b6acbd35cde7bd1
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-04-27 18:22:43 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'parser': '@typescript-eslint/parser',
+    'plugins': ['@typescript-eslint/eslint-plugin'],
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true
+	},
+	// 'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'brace-style': ['error', 'allman'],
+		'indent': ['error', 4],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always']
+	}
+};
